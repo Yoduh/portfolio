cd69813a356b7bc74c418cdd626c61e2f3a3b686
Alex Handlovits <ahandlovits@prometheusgroup.com>
2020-08-07 19:25:27 -0400

    replace twitch clone project with snake game

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ import Fade from 'react-reveal/Fade';
 import ProjectCard from './ProjectCard';
 import rankhorror from './images/rankhorror.png'
 import gelp from './images/gelp.png'
-import twitclone from './images/twitter.png'
+import snake from './images/snake.png'
 
 const Projects = () => {
     return (
@@ -47,11 +47,19 @@ const Projects = () => {
             </MDBCol>
             <MDBCol md="4" className="mb-3">
                 <ProjectCard 
+                    title = "Snake Game"
+                    details = {`The classic snake game that I recreated using React with hooks.`}
+                    image = {snake}
+                    url = 'https://snakeygame.netlify.app/'
+                />
+            </MDBCol>
+            {/* <MDBCol md="4" className="mb-3">
+                <ProjectCard 
                     title = "🚧 Twitter Clone"
                     details = {`I am currently creating a twitter clone for the purpose of teaching myself new web dev skills including Redux Toolkit, Prisma and GraphQL`}
                     image = {twitclone}
                 />
-            </MDBCol>
+            </MDBCol> */}
         </MDBRow>
     </MDBContainer>
     </div>
